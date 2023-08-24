2124966dea6190b2f686c5a408d6e57cc061a365
nikit <Nikitaaa6@yandex.ru>
2023-08-24 22:23:20 +0800

    fist commit

---
+++ src/main/java/com/example/springandmockito1/SpringandMockito1Application.java
@@ -0,0 +1,13 @@
+package com.example.springandmockito1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringandMockito1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringandMockito1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springandmockito1/SpringandMockito1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springandmockito1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringandMockito1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
